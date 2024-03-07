d16d10bb4e067e6f598abfac72effa30dce23571
Loke S. G. Jørgensen <lsgj21@student.aau.dk>
2024-03-07 17:20:51 +0100

    Improved lexer tests so they don't have to be manually rewritten every time new grammar files are generated

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -2,30 +2,53 @@ from grammar_files.generated.RogueLangLexer import RogueLangLexer
 from antlr4 import CommonTokenStream, InputStream
 
 
-def get_tokens_from_str(str):
+def get_tokens_from_string(str):
     lexer = RogueLangLexer(InputStream(str))
     stream = CommonTokenStream(lexer)
     stream.fill()
     tokens = stream.tokens
     return tokens
 
-def test_lexer_ID():
-    code = 'myVar'
-    tokens = get_tokens_from_str(code)
+def get_token_number(token_name):
+    lexer_file = open('src/grammar_files/generated/RogueLang.tokens', 'r')
+    lines = lexer_file.readlines()
 
-    assert tokens[0].type == 16
-    assert tokens[1].type == -1
+    # I'm not sure this method will work for every future lexer rule, but currently it works
+    for line in lines:
+        if token_name in line:
+            token_number = line.partition('=')
+            return int(token_number[2].strip())
+
+    return None
+
+
+def test_lexer_ID():
+    code = 'my_var1'
+    tokens = get_tokens_from_string(code)
+
+    # Doing it this way means if token order changes, we have to rewrite the tests
+    assert tokens[0].type == get_token_number('ID')
+    assert tokens[1].type == -1 # -1 corresponds to type EOF
 
 def test_lexer_INT():
     code = '1'
-    tokens = get_tokens_from_str(code)
+    tokens = get_tokens_from_string(code)
 
-    assert tokens[0].type == 17 # INT is currently type 17
-    assert tokens[1].type == -1 # -1 corresponds to type EOF
+    assert tokens[0].type == get_token_number('INT')
+    assert tokens[1].type == -1
 
 def test_lexer_STRING():
     code = '"hello"'
-    tokens = get_tokens_from_str(code)
+    tokens = get_tokens_from_string(code)
 
-    assert tokens[0].type == 18
+    assert tokens[0].type == get_token_number('STRING')
     assert tokens[1].type == -1
+
+def test_lexer_multiple_tokens():
+    code = 'my_var1 1 "hello"'
+    tokens = get_tokens_from_string(code)
+
+    assert tokens[0].type == get_token_number('ID')
+    assert tokens[1].type == get_token_number('INT')
+    assert tokens[2].type == get_token_number('STRING')
+    assert tokens[3].type == -1
